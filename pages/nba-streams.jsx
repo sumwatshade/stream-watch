1ae991a8316852e104d200d1d7811fb76d1bee35
Luke Shadler <lshadler13@gmail.com>
2019-04-05 00:51:44 -0700

    cleanup nba-streams

--- pages/nba-streams.jsx
+++ pages/nba-streams.jsx
@@ -1,4 +1,5 @@
-import fetch from 'isomorphic-unfetch';
+import React from 'react';
+// import fetch from 'isomorphic-unfetch';
 
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
@@ -13,39 +14,44 @@ const styles = theme => ({
     backgroundColor: theme.palette.background,
   },
 });
-const Index = props => (
-  <Grid container direction="column" justify="center" alignItems="center" spacing={8} className={props.classes.root}>
-    <Grid item>
-      <Typography variant="h1">NBA Games</Typography>
+const Index = (props) => {
+  const { classes: { root }, streamData } = props;
+  return (
+    <Grid container direction="column" justify="center" alignItems="center" spacing={8} className={root}>
+      <Grid item>
+        <Typography variant="h1">NBA Games</Typography>
+      </Grid>
+      {streamData.map(o => (o.title.indexOf('Game Thread') < 0 ? null : (
+        <Game key={`game-${o.title}`} gameData={o} />
+      )))}
     </Grid>
-    {props.streamData.map((o, i) => (o.title.indexOf('Game Thread') < 0 ? null : (
-      <Game key={`game-${i}`} gameData={o} />
-    )))}
-  </Grid>
-);
+  );
+};
 
-Index.getInitialProps = async function () {
+Index.getInitialProps = async function getInit() {
   // const res = await fetch('https://www.reddit.com/r/nbastreams.json')
   const res = mockRes;
   const jsonResponse = await res.json();
 
   const posts = jsonResponse.data.children.map(c => c.data);
 
-  let index = 0;
-  for (const p of posts) {
-    const url = `${p.url.substring(0, p.url.length - 1)}.json`;
-    const commentsRes = await fetch(url);
+  const postsArray = [];
+  posts.forEach(async (p, index) => {
+    const r = Object.assign({}, p);
+    // const url = `${p.url.substring(0, p.url.length - 1)}.json`;
+    // const commentsRes = await fetch(url);
     // const jsonComments = await commentsRes.json();
     const jsonComments = mockPosts[index];
     const comments = jsonComments[1].data.children.map(c => c.data);
 
-    p.comments = comments;
-    index++;
-  }
+    r.comments = comments;
+    postsArray.push(r);
+  });
+
 
   // For each game, find a comment made by the bot that posts stream data
   return {
-    streamData: posts,
+    streamData: postsArray,
     raw: jsonResponse,
   };
 };
